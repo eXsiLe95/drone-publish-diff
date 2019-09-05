4a50f40ef3de4bbe6b2dccb6b80cd8d16b14dd4d
Sebb <datsepp@gmail.com>
2019-09-05 15:51:17 +0200

    Updated compare.sh

--- compare.sh
+++ compare.sh
@@ -1,51 +1,60 @@
 #!/bin/bash
 
 # Check if master file is set
-if [ -z "$CMP_MASTER_FILE" ]; then
-    echo "Master file not set"
+if [ -z "$CMP_MASTER_FOLDER" ]; then
+    echo "Master folder hasn't been set"
     exit 1
 fi
 
 # Check if slave file is set
-if [ -z "$CMP_SLAVE_FILE" ]; then
-    echo "Slave file not set"
+if [ -z "$CMP_SLAVE_FOLDER" ]; then
+    echo "Slave folder hasn't been set"
     exit 1
 fi
 
 # Check if archive name is set
 if [ -z "$CMP_ARCHIVE" ]; then
-    echo "Archive name not set"
+    echo "Build Archive name hasn't been set"
     exit 1
 fi
 
-# Create directories
+
+
+# Declare temp directories
 CMP_TMP="tmp"
-CMP_MASTER_DIR="tmp/master"
-CMP_SLAVE_DIR="tmp/slave"
-CMP_BUILD_DIR="tmp/build"
-CMP_DIFF_LOG="tmp/diff.log"
-CMP_ONLY_IN_MASTER_LOG="tmp/only_in_master.log"
-CMP_ONLY_IN_SLAVE_LOG="tmp/only_in_slave.log"
-CMP_DIFFERS_FROM_LOG="tmp/differs_from.log"
+CMP_MASTER_DIR="$CMP_TMP/master"
+CMP_SLAVE_DIR="$CMP_TMP/slave"
+CMP_BUILD_DIR="$CMP_TMP/build"
+CMP_DIFF_LOG="$CMP_TMP/diff.log"
+CMP_ONLY_IN_MASTER_LOG="$CMP_TMP/only_in_master.log"
+CMP_ONLY_IN_SLAVE_LOG="$CMP_TMP/only_in_slave.log"
+CMP_DIFFERS_FROM_LOG="$CMP_TMP/differs_from.log"
+
+# Remove existent temp directory
+rm -rf $CMP_TMP
+
+# Create temp directories
 mkdir $CMP_TMP
 mkdir $CMP_MASTER_DIR
 mkdir $CMP_SLAVE_DIR
 
-# Unzip archives to compare them
-unzip $CMP_MASTER_FILE -d $CMP_MASTER_DIR
-unzip $CMP_SLAVE_FILE -d $CMP_SLAVE_DIR
-
 # Compare files/directories
-diff -qr $CMP_MASTER_DIR $CMP_SLAVE_DIR >> $CMP_DIFF_LOG
+echo "Finding differences between builds..."
+	diff --exclude=node_modules \
+             --exclude=build \
+             --exclude=administrator/components/com_media/node_modules \
+        $CMP_MASTER_FOLDER $CMP_SLAVE_FOLDER >> $CMP_DIFF_LOG
 
 # Create list of files that only exist in master directory
-cat $CMP_DIFF_LOG | grep -E "^Only in "$CMP_MASTER_DIR"*" | sed -n 's/://p' | awk '{print $3"/"$4}' >> $CMP_ONLY_IN_MASTER_LOG
+cat $CMP_DIFF_LOG | grep -E "^Only in "$CMP_MASTER_FOLDER"*" | sed -n 's/\/://p' |  awk '{print $3"/"$4}' >> $CMP_ONLY_IN_MASTER_LOG
 
 # Create list of files that only exist in slave directory
-cat $CMP_DIFF_LOG | grep -E "^Only in "$CMP_SLAVE_DIR"*" | sed -n 's/://p' | awk '{print $3"/"$4}' >> $CMP_ONLY_IN_SLAVE_LOG
+cat $CMP_DIFF_LOG | grep -E "^Only in "$CMP_SLAVE_FOLDER"*" | sed -n 's/\/://p' | awk '{print $3"/"$4}' >> $CMP_ONLY_IN_SLAVE_LOG
 
 # Create list of files that differ from master
 cat $CMP_DIFF_LOG | grep -Eo " and tmp\/slave\/[^[:space:]]+" | grep -Eo "tmp\/slave\/[^[:space:]]+" >> $CMP_DIFFERS_FROM_LOG
+
+echo "Finished finding differences between builds."
 
 # Create build folder
 mkdir $CMP_BUILD_DIR
@@ -72,7 +81,9 @@ do
 done < $CMP_DIFFERS_FROM_LOG
 
 # Zip build folder
+echo "Zipping build folder..."
 zip -qr $CMP_ARCHIVE $CMP_BUILD_DIR
+echo "Finished zipping build."
 
 # Clean up temporary files
 # rm -rf $CMP_TMP
